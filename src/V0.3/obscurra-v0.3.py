936ec39d6e97316ff6a58058cf62d6b1ff57f8ec
John <john@jakujobi.com>
2024-05-27 19:37:53 -0500

    Added new method to choose models
    - Added choose_model Method: This method allows you to specify which models to use (mtcnn, frontalface, profileface) and calls the corresponding detection methods.
    
    - Separated Detection Methods: Created separate methods for each detection model: detect_faces for Haar cascades and detect_faces_mtcnn for MTCNN.
    
    - Updated process_single_image and process_all_images Methods: These methods now call choose_model to determine which models to use for face detection.
    
    - MainProgram run Method: Updated to accept a list of models to use for detection.

--- src/V0.3/obscurra-v0.3.py
+++ src/V0.3/obscurra-v0.3.py
@@ -3,7 +3,6 @@
 """
 Obscurra v0.3
 
-
 License:
     This project is licensed under the HPL 3 License - see the LICENSE file for details.
 
@@ -11,12 +10,9 @@ Author:
     John Akujobi (john@jakujobi.com)
 """
 
-
 import cv2
 import glob
 import os
-import mtcnn
-import tensorflow
 from mtcnn import MTCNN
 
 
@@ -25,19 +21,9 @@ class DirectoryManager:
 
     @staticmethod
     def get_current_directory():
-        """
-        Get the current directory.
-
-        Returns:
-            str: The current directory path.
-
-        Raises:
-            Exception: If there is an error getting the current directory.
-        """
         try:
             current_directory = os.path.dirname(os.path.realpath(__file__))
             print("Getting current directory..." + current_directory)
-            # return current_directory + '/test'
             return current_directory + DirectoryManager.TEST_DIR_SUFFIX
         except Exception as e:
             print(f"Error getting current directory: {e}")
@@ -58,9 +44,6 @@ class ImageProcessor:
     BLUR_EFFECT = (30, 30)
     FRONT_FACE_CASCADE_PATH = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
     PROFILE_FACE_CASCADE_PATH = cv2.data.haarcascades + 'haarcascade_profileface.xml'
-    SCALE_FACTOR = 1.05
-    MIN_NEIGHBORS = 3
-    MIN_SIZE = (30, 30)
 
     def __init__(self):
         self._front_face_cascade = self._load_face_detection_model(self.FRONT_FACE_CASCADE_PATH)
@@ -88,13 +71,41 @@ class ImageProcessor:
     def mtcnn_detector(self):
         return self._mtcnn_detector
 
+    def choose_model(self, models, image, gray_image):
+        faces = []
+        if 'mtcnn' in models:
+            faces_mtcnn = self.detect_faces_mtcnn(image)
+            faces.extend(faces_mtcnn)
+        if 'frontalface' in models:
+            faces_frontal = self.detect_faces(gray_image, self.front_face_cascade)
+            faces.extend(faces_frontal)
+        if 'profileface' in models:
+            faces_profile = self.detect_faces(gray_image, self.profile_face_cascade)
+            faces.extend(faces_profile)
+        # Remove duplicates
+        faces = list(set(map(tuple, faces)))
+        faces = [list(face) for face in faces]
+        return faces
+
     @staticmethod
     def detect_faces(gray, face_cascade):
         try:
-            faces = face_cascade.detectMultiScale(gray, scaleFactor=ImageProcessor.SCALE_FACTOR, minNeighbors=ImageProcessor.MIN_NEIGHBORS, minSize=ImageProcessor.MIN_SIZE)
+            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
             return faces
         except Exception as e:
             print(f"Error detecting faces: {e}")
+            raise e
+
+    def detect_faces_mtcnn(self, image):
+        try:
+            results = self.mtcnn_detector.detect_faces(image)
+            faces = []
+            for result in results:
+                x, y, width, height = result['box']
+                faces.append((x, y, width, height))
+            return faces
+        except Exception as e:
+            print(f"Error detecting faces with MTCNN: {e}")
             raise e
 
     @staticmethod
@@ -109,51 +120,26 @@ class ImageProcessor:
         base_name = os.path.basename(image_path)
         name, ext = os.path.splitext(base_name)
         return os.path.join(output_folder, f"{name}_b{ext}")
-    
+
     @staticmethod
     def preprocess_image(image):
-        # Convert to grayscale
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        # Equalize histogram
         gray = cv2.equalizeHist(gray)
-
-        # Resize image (if necessary)
-        # gray = cv2.resize(gray, (new_width, new_height))
-
         return gray
 
-    @staticmethod
-    def process_single_image(image_path, output_folder, front_face_cascade, profile_face_cascade, mtcnn_detector):
+    def process_single_image(self, image_path, output_folder, models):
         try:
             print(f"Processing {image_path}")
-            img = ImageProcessor.read_image(image_path)
-            gray = ImageProcessor.preprocess_image(img)  # Call the new method here
-            faces_front = ImageProcessor.detect_faces(gray, front_face_cascade)
-            faces_profile = ImageProcessor.detect_faces(gray, profile_face_cascade)
-
-            # Check if both lists are not empty
-            if len(faces_front) > 0 and len(faces_profile) > 0:
-                faces = list(set(map(tuple, faces_front)) | set(map(tuple, faces_profile)))
-                faces = [list(face) for face in faces]
-            elif len(faces_front) > 0:
-                faces = faces_front
-            elif len(faces_profile) > 0:
-                faces = faces_profile
-            else:
-                faces = []
+            img = self.read_image(image_path)
+            gray = self.preprocess_image(img)
+            faces = self.choose_model(models, img, gray)
 
             if len(faces) > 0:
                 print(f"Detected {len(faces)} face(s) in {image_path}.")
-                # Use MTCNN on the detected areas
-                for (x, y, w, h) in faces:
-                    face_img = img[y:y+h, x:x+w]
-                    result = mtcnn_detector.detect_faces(face_img)
-                    if result:  # If MTCNN detects a face
-                        ImageProcessor.blur_faces(face_img, [(result[0]['box'][0], result[0]['box'][1], result[0]['box'][2], result[0]['box'][3])])
+                self.blur_faces(img, faces)
             else:
                 print(f"No faces detected in {image_path}.")
-            output_path = ImageProcessor.get_output_path(image_path, output_folder)
+            output_path = self.get_output_path(image_path, output_folder)
             cv2.imwrite(output_path, img)
             print(f"Processed and saved {output_path}")
         except Exception as e:
@@ -161,35 +147,25 @@ class ImageProcessor:
             raise e
 
     @staticmethod
-    def detect_faces(gray, face_cascade):
-        try:
-            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-            return faces
-        except Exception as e:
-            print(f"Error detecting faces: {e}")
-            raise e
-
-    @staticmethod
     def blur_faces(img, faces):
         try:
             for (x, y, w, h) in faces:
-                # Apply blur effect to the face region
                 img[y:y+h, x:x+w] = cv2.blur(img[y:y+h, x:x+w], ImageProcessor.BLUR_EFFECT)
             return img
         except Exception as e:
             print(f"Error blurring faces: {e}")
             raise e
 
-    @staticmethod
-    def process_all_images(input_folder, output_folder, front_face_cascade, profile_face_cascade, mtcnn_detector):
+    def process_all_images(self, input_folder, output_folder, models):
         try:
-            for extension in ImageProcessor.IMAGE_EXTENSIONS:
+            for extension in self.IMAGE_EXTENSIONS:
                 for filename in glob.glob(os.path.join(input_folder, extension)):
-                    ImageProcessor.process_single_image(filename, output_folder, front_face_cascade, profile_face_cascade, mtcnn_detector)
+                    self.process_single_image(filename, output_folder, models)
             print("Face blurring complete.")
         except Exception as e:
             print(f"Error processing all images: {e}")
             raise e
+
 
 class MainProgram:
     OUTPUT_FOLDER_NAME = 'blurred'
@@ -198,25 +174,19 @@ class MainProgram:
         self.directory_manager = DirectoryManager()
         self.image_processor = ImageProcessor()
 
-    def run(self):
+    def run(self, models):
         try:
-            # Get the current directory
             current_directory = self.directory_manager.get_current_directory()
-
-            # Path to the input and output folders
             input_folder = current_directory
             output_folder = os.path.join(current_directory, MainProgram.OUTPUT_FOLDER_NAME)
-
-            # Create the output folder
             self.directory_manager.create_output_directory(output_folder)
-
-            # Process all images
-            self.image_processor.process_all_images(input_folder, output_folder, self.image_processor.front_face_cascade, self.image_processor.profile_face_cascade, self.image_processor.mtcnn_detector)
+            self.image_processor.process_all_images(input_folder, output_folder, models)
         except Exception as e:
             print(f"Error running the program: {e}")
             raise e
 
+
 if __name__ == "__main__":
-    # Run the main program
     main_program = MainProgram()
-    main_program.run()
+    models_to_use = ['mtcnn', 'frontalface', 'profileface']  # Specify which models to use
+    main_program.run(models_to_use)
