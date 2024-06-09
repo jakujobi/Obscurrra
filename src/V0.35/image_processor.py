20410d182720fb2df2145e854a3abd590a707581
John <john@jakujobi.com>
2024-06-09 00:55:42 -0500

    Split image processor into multiple classes in the same file

--- src/V0.35/image_processor.py
+++ src/V0.35/image_processor.py
@@ -1,5 +1,3 @@
-# src/image_processor.py
-
 import cv2
 import glob
 import os
@@ -7,28 +5,38 @@ import logging
 from mtcnn import MTCNN
 from concurrent.futures import ThreadPoolExecutor
 
-class ImageProcessor:
-    IMAGE_EXTENSIONS = ['*.jpg', '*.jpeg', '*.png', '*.webp']
-    BLUR_EFFECT = (70, 70)
+class Preprocessor:
+    @staticmethod
+    def read_image(image_path):
+        img = cv2.imread(image_path)
+        if img is None:
+            raise ValueError(f"Error reading image {image_path}.")
+        return img
+
+    @staticmethod
+    def resize_image(image, max_dimension):
+        height, width = image.shape[:2]
+        if max(height, width) > max_dimension:
+            scale = max_dimension / max(height, width)
+            new_size = (int(width * scale), int(height * scale))
+            resized_image = cv2.resize(image, new_size, interpolation=cv2.INTER_AREA)
+            return resized_image
+        return image
+
+    @staticmethod
+    def preprocess_image(image):
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray = cv2.equalizeHist(gray)
+        return gray
+
+class FaceDetection:
     FRONT_FACE_CASCADE_PATH = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
     PROFILE_FACE_CASCADE_PATH = cv2.data.haarcascades + 'haarcascade_profileface.xml'
-    _MAX_IMAGE_SIZE = 1000  # Maximum size of the image's largest dimension
 
     def __init__(self):
         self._front_face_cascade = self._load_face_detection_model(self.FRONT_FACE_CASCADE_PATH)
         self._profile_face_cascade = self._load_face_detection_model(self.PROFILE_FACE_CASCADE_PATH)
         self._mtcnn_detector = MTCNN()
-
-    @property
-    def max_image_size(self):
-        return self._MAX_IMAGE_SIZE
-
-    @max_image_size.setter
-    def max_image_size(self, value):
-        if value > 0:
-            self._MAX_IMAGE_SIZE = value
-        else:
-            raise ValueError("Maximum image size must be greater than 0.")
 
     @staticmethod
     def _load_face_detection_model(model_path):
@@ -65,7 +73,7 @@ class ImageProcessor:
     def filter_faces(new_faces, existing_faces):
         filtered_faces = []
         for face in new_faces:
-            if not any(ImageProcessor.is_same_face(face, existing_face) for existing_face in existing_faces):
+            if not any(FaceDetection.is_same_face(face, existing_face) for existing_face in existing_faces):
                 filtered_faces.append(face)
         return filtered_faces
 
@@ -93,12 +101,38 @@ class ImageProcessor:
             logging.error(f"Error detecting faces with MTCNN: {e}")
             raise e
 
+class FaceBlurrer:
+    BLUR_EFFECT = (70, 70)
+
     @staticmethod
-    def read_image(image_path):
-        img = cv2.imread(image_path)
-        if img is None:
-            raise ValueError(f"Error reading image {image_path}.")
-        return img
+    def blur_faces(img, faces):
+        try:
+            for (x, y, w, h) in faces:
+                img[y:y+h, x:x+w] = cv2.blur(img[y:y+h, x:x+w], FaceBlurrer.BLUR_EFFECT)
+            return img
+        except Exception as e:
+            logging.error(f"Error blurring faces: {e}")
+            raise e
+
+class ImageProcessor:
+    IMAGE_EXTENSIONS = ['*.jpg', '*.jpeg', '*.png', '*.webp']
+    _MAX_IMAGE_SIZE = 1000  # Maximum size of the image's largest dimension
+
+    def __init__(self):
+        self.preprocessor = Preprocessor()
+        self.face_detection = FaceDetection()
+        self.face_blurrer = FaceBlurrer()
+
+    @property
+    def max_image_size(self):
+        return self._MAX_IMAGE_SIZE
+
+    @max_image_size.setter
+    def max_image_size(self, value):
+        if value > 0:
+            self._MAX_IMAGE_SIZE = value
+        else:
+            raise ValueError("Maximum image size must be greater than 0.")
 
     @staticmethod
     def get_output_path(image_path, output_folder):
@@ -106,29 +140,13 @@ class ImageProcessor:
         name, ext = os.path.splitext(base_name)
         return os.path.join(output_folder, f"{name}_b{ext}")
 
-    @staticmethod
-    def preprocess_image(image):
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        gray = cv2.equalizeHist(gray)
-        return gray
-    
-    @staticmethod
-    def resize_image(image, max_dimension):
-        height, width = image.shape[:2]
-        if max(height, width) > max_dimension:
-            scale = max_dimension / max(height, width)
-            new_size = (int(width * scale), int(height * scale))
-            resized_image = cv2.resize(image, new_size, interpolation=cv2.INTER_AREA)
-            return resized_image
-        return image
-
     def process_single_image(self, image_path, output_folder, models):
         try:
             logging.info(f"Processing {image_path}")
-            original_img = self.read_image(image_path)
-            resized_img = self.resize_image(original_img.copy(), self.max_image_size)  # Resize the copy for face detection
-            gray = self.preprocess_image(resized_img)
-            faces = self.choose_model(models, resized_img, gray)
+            original_img = self.preprocessor.read_image(image_path)
+            resized_img = self.preprocessor.resize_image(original_img.copy(), self.max_image_size)  # Resize the copy for face detection
+            gray = self.preprocessor.preprocess_image(resized_img)
+            faces = self.face_detection.choose_model(models, resized_img, gray)
 
             # Scale factor for translating face coordinates back to original image size
             scale_factor = max(original_img.shape[:2]) / max(resized_img.shape[:2])
@@ -137,7 +155,7 @@ class ImageProcessor:
                 logging.info(f"Detected {len(faces)} face(s) in {image_path}.")
                 # Scale the face coordinates back to original image size
                 faces = [(int(x*scale_factor), int(y*scale_factor), int(w*scale_factor), int(h*scale_factor)) for (x, y, w, h) in faces]
-                self.blur_faces(original_img, faces)
+                self.face_blurrer.blur_faces(original_img, faces)
             else:
                 logging.info(f"No faces detected in {image_path}.")
             output_path = self.get_output_path(image_path, output_folder)
@@ -145,16 +163,6 @@ class ImageProcessor:
             logging.info(f"Processed and saved {output_path}")
         except Exception as e:
             logging.error(f"Error processing {image_path}: {e}")
-            raise e
-
-    @staticmethod
-    def blur_faces(img, faces):
-        try:
-            for (x, y, w, h) in faces:
-                img[y:y+h, x:x+w] = cv2.blur(img[y:y+h, x:x+w], ImageProcessor.BLUR_EFFECT)
-            return img
-        except Exception as e:
-            logging.error(f"Error blurring faces: {e}")
             raise e
 
     def process_all_images(self, input_folder, output_folder, models):
